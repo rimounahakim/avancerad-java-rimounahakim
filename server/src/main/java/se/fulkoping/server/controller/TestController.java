1e895b85cd09f3dc8db2a5ddd3eb3f9774763832
Rimouna Al Hakim <rimouna14hakim@gmail.com>
2025-12-26 06:04:05 +0100

    Add Get and Post endpoints for Annons

--- server/src/main/java/se/fulkoping/server/controller/TestController.java
+++ server/src/main/java/se/fulkoping/server/controller/TestController.java
@@ -1,19 +1,38 @@
 package se.fulkoping.server.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import se.fulkoping.server.repository.AnnonsRepository;
 
-// Enkel controller för att testa om servern fungerar rätt
+import se.fulkoping.server.repository.AnnonsRepository;
+import se.fulkoping.server.model.Annons;
+
+import java.util.List;
+
+// Enkel controller för att testa och hantera annonser
 @RestController
 public class TestController {
 
-    // Repository för att hantera annonser (lagras i minnet)
+    // Repository som lagrar annonser i minnet
     private final AnnonsRepository repository = new AnnonsRepository();
 
-    // Test-endpoint för att kontrollera att servern svarar
+    // Test-endpoint för att se om servern fungerar
     @GetMapping("/test")
     public String test() {
         return "Servern fungerar!";
+    }
+
+    // Endpoint för att hämta alla annonser
+    @GetMapping("/annons")
+    public List<Annons> getAllAnnons() {
+        return repository.findAll();
+    }
+
+    // Endpoint för att lägga till en ny annons
+    @PostMapping("/annons")
+    public String addAnnons(@RequestBody Annons annons) {
+        repository.save(annons);
+        return "Annons sparad!";
     }
 }
